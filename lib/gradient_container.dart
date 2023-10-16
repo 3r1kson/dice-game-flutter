a625592bb5efa5859292d11dceea50a1cb212bb2
Erikson <eriksonribeiro84@gmail.com>
2023-10-16 19:38:59 -0300

    first commit

---
+++ lib/gradient_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAligment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key}); // constructor
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [color1, color2], begin: startAlignment, end: endAligment),
+      ),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/dice-2.png',
+              width: 200,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
